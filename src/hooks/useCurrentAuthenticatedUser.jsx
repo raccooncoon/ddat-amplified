69a8c19525f32a74e44e9171d4b2de352a836d72
임민영 <raccoon@shinsegae.com>
2024-03-21 15:52:33 +0900

    그래바타 추가

--- src/hooks/useCurrentAuthenticatedUser.jsx
+++ src/hooks/useCurrentAuthenticatedUser.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {getCurrentUser} from "aws-amplify/auth";
-import {md5} from "@mui/x-license-pro/encoding/md5.js";
+import md5 from "md5";
 
 const UseCurrentAuthenticatedUser = () => {
   const [user, setUser] = useState({});
@@ -14,8 +14,8 @@ const UseCurrentAuthenticatedUser = () => {
         setUser({
           userName: userData?.signInDetails?.loginId.split("@")[0],
           email: userData?.signInDetails?.loginId,
-          url: `https://www.gravatar.com/avatar/${ md5(
-              userData?.signInDetails?.loginId?.trim()?.toLowerCase())}?size=512`
+          url: `https://www.gravatar.com/avatar/${md5(
+              userData?.signInDetails?.loginId?.trim()?.toLowerCase())}?d=retro:`
         });
       } catch (error) {
         console.error('Error fetching user data:', error);
