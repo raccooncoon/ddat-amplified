b4c300b2b663b4c49ffba47354bd9ae3af4cadb7
임민영 <raccoon@shinsegae.com>
2024-03-25 11:56:38 +0900

    graqhql 제거 업로드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,27 @@ import Layout from "./scenes/layout/index.jsx";
 import XmlFiles from "./scenes/XmlFiles/index.jsx";
 import {withAuthenticator} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import {QueryClient, QueryClientProvider} from "react-query";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const queryClient = new QueryClient();
   return (
       <div className="app">
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <Routes>
-              <Route element={<Layout/>}>
-                <Route path="/" element={<Navigate to="/xmlFiles" replace />}/>
-                <Route path="/xmlFiles" element={<XmlFiles/>}/>
-              </Route>
-            </Routes>
-          </ThemeProvider>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <ThemeProvider theme={theme}>
+              <CssBaseline/>
+              <Routes>
+                <Route element={<Layout/>}>
+                  <Route path="/" element={<Navigate to="/xmlFiles" replace/>}/>
+                  <Route path="/xmlFiles" element={<XmlFiles/>}/>
+                </Route>
+              </Routes>
+            </ThemeProvider>
+          </BrowserRouter>
+        </QueryClientProvider>
       </div>
   )
 }
--- src/components/UploadDataToGraphQL.jsx
+++ src/components/UploadDataToGraphQL.jsx
@@ -2,100 +2,84 @@ import {styled} from "@mui/system";
 import {Box, useTheme} from "@mui/material";
 import {CloudUploadOutlined} from "@mui/icons-material";
 import LoadingButton from '@mui/lab/LoadingButton';
-import {generateClient} from 'aws-amplify/api';
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function UploadDataToGraphQL() {
-    const theme = useTheme();
-    // const [selectedFile, setSelectedFile] = useState(null); // 파일 상태를 저장하는 selectedFile state를 추가합니다.
-    const [loading, setLoading] = useState(false);
-    const client = generateClient(); // AWS Amplify API 클라이언트를 생성합니다.
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        // setSelectedFile(file);
-
-        // 파일을 읽기 위한 FileReader 객체 생성
-        const reader = new FileReader();
-
-        // 파일 읽기가 완료되면 실행될 콜백 함수 정의
-        reader.onload = (e) => {
-
-            const content = JSON.parse(e.target.result);
-            setLoading(true); // 파일 업로드가 시작되면 로딩 상태를 true로 설정합니다.
-            alert('Uploading ' + content.length + ' records')
-            // 시작숫자 마지막숫자 입력 받기, 기본값 설정
-            let start = parseInt(prompt('시작숫자를 입력하세요', 0));
-            let end = parseInt(prompt('마지막숫자를 입력하세요', content.length));
-            content.slice(start, end).forEach(data => {
-                // content.forEach(data => {
-                setTimeout(() => {
-                    sendToGraphQL(data);
-                }, 1000);
-            });
-            setLoading(false);
-        };
-        // 파일을 텍스트로 읽기
-        reader.readAsText(file);
+  const theme = useTheme();
+  const [datas, setDatas] = useState([]);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const content = JSON.parse(e.target.result);
+      alert('Uploading ' + content.length + ' records')
+      // 시작숫자 마지막숫자 입력 받기, 기본값 설정
+      let start = parseInt(prompt('시작숫자를 입력하세요', 0));
+      let end = parseInt(prompt('마지막숫자를 입력하세요', content.length));
+      setDatas(content.slice(start, end));
     };
+    // 파일을 텍스트로 읽기
+    reader.readAsText(file);
+  };
 
-    const sendToGraphQL = async (data) => {
-        // console.table(data)
-        // try {
-        //     const response = await client.graphql({
-        //         query: createXmlModel, variables: {
-        //             input: {
-        //                 moduleName: data.moduleName,
-        //                 xmlid: data.xmlid,
-        //                 namespace: data.namespace,
-        //                 subtag: data.subtag,
-        //                 fileName: data.fileName,
-        //                 context: data.context,
-        //                 urlCount: data.urlCount,
-        //                 MethodModels: data.methodModels
-        //             },
-        //         },
-        //     })
-        //     console.log('GraphQL response:', response);
-        //     // 그래프큐엘 서버로부터 받은 응답을 처리하는 로직을 추가할 수 있습니다.
-        // } catch (error) {
-        //     console.error('Error sending query to GraphQL:', error);
-        //     // 오류 발생 시 처리하는 로직을 추가할 수 있습니다.
-        // }
-    };
+  useEffect(() => {
+    async function fetchData() {
+      for (const data of datas) {
+        await axios.post('http://localhost:8080/api/xml_file/context/', {
+          moduleName: data.moduleName,
+          xmlid: data.xmlid,
+          namespace: data.namespace,
+          subtag: data.subtag,
+          fileName: data.fileName,
+          context: data.context,
+          urlCount: data.urlCount,
+          MethodModels: JSON.stringify(data.methodModels)
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      }
+    }
+    fetchData();
+  }, [datas]);
 
-    const VisuallyHiddenInput = styled('input')({
-        clip: 'rect(0 0 0 0)',
-        clipPath: 'inset(50%)',
-        height: 1,
-        overflow: 'hidden',
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        whiteSpace: 'nowrap',
-        width: 1,
-    });
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
 
-    return (<Box>
-        <LoadingButton loading={loading}
-                       component="label"
-                       role={undefined}
-                       variant="contained"
-                       tabIndex={-1}
-                       startIcon={<CloudUploadOutlined/>}
-                       disabled
-                       sx={{
-                           backgroundColor: theme.palette.secondary.light,
-                           color: theme.palette.background.alt,
-                           fontWeight: "bold",
-                           ":hover": {
-                               backgroundColor: theme.palette.secondary.dark,
-                           }
-                       }}
-        >
-            Upload file
-            <VisuallyHiddenInput type="file" onChange={handleFileChange}/>
-        </LoadingButton>
-    </Box>);
+  return (<Box>
+    <LoadingButton loading={false}
+                   component="label"
+                   role={undefined}
+                   variant="contained"
+                   tabIndex={-1}
+                   startIcon={<CloudUploadOutlined/>}
+                   disabled={false}
+                   sx={{
+                     backgroundColor: theme.palette.secondary.light,
+                     color: theme.palette.background.alt,
+                     fontWeight: "bold",
+                     ":hover": {
+                       backgroundColor: theme.palette.secondary.dark,
+                     }
+                   }}
+    >
+      Upload file
+      <VisuallyHiddenInput type="file" onChange={handleFileChange}/>
+    </LoadingButton>
+  </Box>);
 }
 
 export default UploadDataToGraphQL;
