150a579da7fe87d656a49d78d808a80a187dfe48
임민영 <raccoon@shinsegae.com>
2024-04-04 13:52:37 +0900

    ec2 web hook

---
+++ amplify/backend/function/webhooklambda/src/index.js
@@ -0,0 +1,25 @@
+const axios= require('axios');
+//event.json 파일 읽어 오기
+const fs = require('fs');
+const event = fs.readFileSync('event.json', 'utf8');
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+
+  const { detail } = JSON.parse(event);
+
+  const url = "https://shinsegaegroup.webhook.office.com/webhookb2/947d05b1-dbd6-4f83-92cb-9e26a4c4a59f@d4ffc887-d88d-41cc-bf6a-6bb47ec0f3ca/IncomingWebhook/a54e896ce490485698c2179045e4ee04/319679f3-8914-4f42-a830-d1152eb0b4cf"
+  let axiosResponse = await axios.post(url, {text: JSON.stringify(detail)}, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify(axiosResponse.data)
+  }
+};
+
+exports.handler(event)
